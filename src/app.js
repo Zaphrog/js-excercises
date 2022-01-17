acc1e948613af47435523c1af7052b40ef658522
Zaphrog <diegocottin@gmail.com>
2022-01-17 09:47:10 +0000

    finished excercise

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function excuse() {
+    let who = ["a ninja", "my ex", "my dog", "a time traveler", "kurt cobain"];
+    let what = [
+      "destroyed",
+      "obliterated",
+      "criticized",
+      "ate",
+      "wrote a song about"
+    ];
+    let whom = [
+      "my car",
+      "my motivation",
+      "the meaning of it all",
+      "me",
+      "everything"
+    ];
+    let when = [
+      "today",
+      "a few minutes ago",
+      "during the weekend",
+      "yesterday",
+      "...ummmm it will happen in a minute or so"
+    ];
+    let excuse = "";
+    excuse = excuse + who[Math.floor(Math.random() * 5)] + " ";
+    excuse = excuse + what[Math.floor(Math.random() * 5)] + " ";
+    excuse = excuse + whom[Math.floor(Math.random() * 5)] + " ";
+    excuse = excuse + when[Math.floor(Math.random() * 5)];
+    return excuse;
+  }
+  document.querySelector("#generate").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuse();
+  });
+
+  console.log("hello!");
 };
